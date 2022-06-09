e016588a78bfd09713c079a4590784aecbc4d712
aliNejati-bu <electrocellco@gmail.com>
2022-06-09 10:04:30 +0430

    add version router

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -1,13 +1,14 @@
 import {Express} from "express";
 import {router} from "./routerLogger";
+import api from "./api";
 
 export class Router {
-
     init(app: Express) {
         app.use(router);
 
+        // api router
+        app.use("/api", api);
     }
-
 }
 
 export default new Router();
